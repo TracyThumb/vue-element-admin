a21bbb589bbc3ac1bd51e92bca8971c57762cf9e
vincent <ongbinghui@gmail.com>
2019-07-18 10:33:33 +0800

    fixed sone npm install problems

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: function() {
+    return {
+      textEslint: 'dasdf',
+      try: 'sdf',
+      asdf: 'lvoe'
+    }
+  }
 }
 </script>
